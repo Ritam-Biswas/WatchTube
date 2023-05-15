5187e91410daa521d23024b6c76826649ca72482
Ritam Biswas <ritambiswas1801@gmail.com>
2023-05-15 22:28:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { ChannelDetail, Feed, Navbar, VideoDetail } from './components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <>
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Feed/>}/>
+            <Route path="/videos/:id" element={<VideoDetail/>}/>
+            <Route path="/channels/:id" element={<ChannelDetail/>}/>
+            <Route path="/" element={<Feed/>}/>
+            <Route path="/" element={<Feed/>}/>
+        </Routes>
+    </>
+)
 
 export default App;
